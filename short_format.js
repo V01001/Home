7f6a429c0ecee00de098e7aa79ac5ba8ed61e3fc
unknown <primeval@mail.ru>
2023-05-02 13:04:10 +0200

    asa

---
+++ short_format.js
@@ -0,0 +1,12 @@
+const name = 'Valek'
+const postQty = 14
+const testQty = 111
+
+
+const userProfile = {
+    name: name,
+    postQty: postQty,
+    hasSignedAgreement: false    
+}
+
+console.log(userProfile)
